7d577486d91bd4519e731204bbdf8428e4e0b853
C.Even <fountainment@gmail.com>
2015-06-22 18:34:15 +0800

    edit the editor

--- tools/codeFormat.sh
+++ tools/codeFormat.sh
@@ -1,4 +1,4 @@
 ePath=..
-file=`find $ePath/*.cpp $ePath/*.h $ePath/include/*.h $ePath/include/fountain/*.h| grep -v '~'`
+file=`find $ePath/*.cpp $ePath/*.h $ePath/include/*.h $ePath/include/fountain/*.h $ePath/FSMEditor/*.cpp $ePath/FSMEditor/*.h| grep -v '~'`
 formatCom='astyle --indent=tab'
 $formatCom $file
